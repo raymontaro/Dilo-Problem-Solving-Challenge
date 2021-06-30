6e1e02cf8d590f854799b644d673c72f7d80a29a
raymontaro <mega.ray.mon93@gmail.com>
2021-06-30 10:19:15 +0700

    Problem2

---
+++ Dilo Problem Solving Challenge/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speedX;
+    public float speedZ;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+
+        rb.AddForce(new Vector3(speedX, 0, speedZ), ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
