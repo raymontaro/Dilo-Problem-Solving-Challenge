6b9a1b2812a60f2ac88e1aa8c575dced9fff38d7
raymontaro <mega.ray.mon93@gmail.com>
2021-06-30 13:48:42 +0700

    Problem10

---
+++ Dilo Problem Solving Challenge/Assets/Scripts/OpenBuild.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System;
+
+public class OpenBuild : MonoBehaviour
+{
+    public void OpenProblem1()
+    {
+        System.Diagnostics.Process.Start(Environment.CurrentDirectory + @"\..\Problem1\Dilo Problem Solving Challenge.exe");
+    }
+
+    public void OpenProblem2()
+    {
+        System.Diagnostics.Process.Start(Environment.CurrentDirectory + @"\..\Problem2\Dilo Problem Solving Challenge.exe");
+    }
+
+    public void OpenProblem3()
+    {
+        System.Diagnostics.Process.Start(Environment.CurrentDirectory + @"\..\Problem3\Dilo Problem Solving Challenge.exe");
+    }
+
+    public void OpenProblem4()
+    {
+        System.Diagnostics.Process.Start(Environment.CurrentDirectory + @"\..\Problem4\Dilo Problem Solving Challenge.exe");
+    }
+
+    public void OpenProblem5()
+    {
+        System.Diagnostics.Process.Start(Environment.CurrentDirectory + @"\..\Problem5\Dilo Problem Solving Challenge.exe");
+    }
+
+    public void OpenProblem6()
+    {
+        System.Diagnostics.Process.Start(Environment.CurrentDirectory + @"\..\Problem6\Dilo Problem Solving Challenge.exe");
+    }
+
+    public void OpenProblem7()
+    {
+        System.Diagnostics.Process.Start(Environment.CurrentDirectory + @"\..\Problem7\Dilo Problem Solving Challenge.exe");
+    }
+
+    public void OpenProblem8()
+    {
+        System.Diagnostics.Process.Start(Environment.CurrentDirectory + @"\..\Problem8\Dilo Problem Solving Challenge.exe");
+    }
+
+    public void OpenProblem9()
+    {
+        System.Diagnostics.Process.Start(Environment.CurrentDirectory + @"\..\Problem9\Dilo Problem Solving Challenge.exe");
+    }
+}
