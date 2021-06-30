fbed74fbe6e54c0e0c1d57e9021efb6d53b40011
raymontaro <mega.ray.mon93@gmail.com>
2021-06-30 11:19:32 +0700

    Problem7

---
+++ Dilo Problem Solving Challenge/Assets/Scripts/Box.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Score.Instance.AddScore(1);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Dilo Problem Solving Challenge/Assets/Scripts/Score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static Score Instance;
+
+    public int currentScore = 0;
+
+    Text text;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    public void AddScore(int value)
+    {
+        currentScore += value;
+
+        text.text = "Score: " + currentScore;
+    }
+}
